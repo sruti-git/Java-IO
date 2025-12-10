f943a4cff81ff783a99eaf5e5ddbcd2539932b1b
sruti-git <srutiselvi0411@gmail.com>
2025-12-10 18:30:53 +0530

    Add files via upload

---
+++ Main (6).java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Fractional Number:");
+        double num=sc.nextDouble();
+        System.out.printf("%.2f",num);
+    }
+}
